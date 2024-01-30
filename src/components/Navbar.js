e9dc7fd960756ab3d7748bab9bed1126720dde6f
Yahya <yahyahussein670@gmail.com>
2024-01-30 07:56:13 +0300

    tweaks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,28 @@
 import React from "react";
-import logo from '../assets/images/logo.webp'
+import logo from '../assets/images/logo.webp';
 
 function Navbar() {
   return (
-      <div className="flex items-center justify-between">
-        <img src={logo} alt="" />
-        <div className="hidden space-x-8 md:flex">
-          <a href="/" className="hover:text-slate-500">
-            Home
-          </a>
-          <a href="/pricing" className="hover:text-slate-500">
-            pricing
-          </a>
-          <a href="/community" className="hover:text-slate-500">
-            community{" "}
-          </a>
-          <a href="/services" className="hover:text-slate-500">
-            services
-          </a>
-        </div>
-        <a href="/start" className="hidden md:block baseline rounded-full bg-green-500 p-3 px-6 pt-2 ">
-          Get Started
+    <div className="flex items-center justify-between">
+      <img src={logo} alt="" />
+      <div className="hidden space-x-8 md:flex">
+        <a href="/" className="hover:text-slate-500">
+          Home
+        </a>
+        <a href="/pricing" className="hover:text-slate-500">
+          Pricing
+        </a>
+        <a href="/community" className="hover:text-slate-500">
+          Community
+        </a>
+        <a href="/services" className="hover:text-slate-500">
+          Services
         </a>
       </div>
+      <a href="/start" className="hidden md:block baseline rounded-full bg-green-500 p-3 px-6 pt-2 ">
+        Get Started
+      </a>
+    </div>
   );
 }
 
