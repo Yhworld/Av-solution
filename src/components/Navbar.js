8f5f8f66fef2154a52361596958e940941795b0c
Yahya <yahyahussein670@gmail.com>
2024-01-30 07:49:05 +0300

    add placeholders for the links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,20 +6,20 @@ function Navbar() {
       <div className="flex items-center justify-between">
         <img src={logo} alt="" />
         <div className="hidden space-x-8 md:flex">
-          <a href="#" className="hover:text-slate-500">
+          <a href="/" className="hover:text-slate-500">
             Home
           </a>
-          <a href="#" className="hover:text-slate-500">
+          <a href="/pricing" className="hover:text-slate-500">
             pricing
           </a>
-          <a href="#" className="hover:text-slate-500">
+          <a href="/community" className="hover:text-slate-500">
             community{" "}
           </a>
-          <a href="#" className="hover:text-slate-500">
+          <a href="/services" className="hover:text-slate-500">
             services
           </a>
         </div>
-        <a href="#" className="hidden md:block baseline rounded-full bg-green-500 p-3 px-6 pt-2 ">
+        <a href="/start" className="hidden md:block baseline rounded-full bg-green-500 p-3 px-6 pt-2 ">
           Get Started
         </a>
       </div>
