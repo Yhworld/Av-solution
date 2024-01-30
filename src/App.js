4c90e827a3a9de2378b0c1a80622cb3a84a41623
Yahya <yahyahussein670@gmail.com>
2024-01-30 07:16:04 +0300

    arrange the overall layer of the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './styles.css';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <>
-    </>
+    <div className="container relative mx-auto p-4">
+    <Navbar />
+    <HomePage />
+    </div>
   );
 }
 
