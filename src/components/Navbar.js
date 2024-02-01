debcc58af80786f86a3a1b1f69ca2084c6e472ad
Yahya <yahyahussein670@gmail.com>
2024-02-01 13:54:09 +0300

    update buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from '../assets/images/logo.webp';
+import logo from "../assets/images/logo.webp";
 
 function Navbar() {
   return (
@@ -19,8 +19,12 @@ function Navbar() {
           Services
         </a>
       </div>
-      <a href="/start" className="hidden md:block baseline rounded-full bg-green-500 p-3 px-6 pt-2 ">
-        Get Started
+      <a href="#_" class="relative hidden md:block px-4 py-2 font-medium group">
+        <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+        <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+        <span class="relative text-black group-hover:text-white">
+          Getting Started
+        </span>
       </a>
     </div>
   );
