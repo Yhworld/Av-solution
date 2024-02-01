f47ecadfa5204053f956e9b9ce95301b05778c51
Yahya <yahyahussein670@gmail.com>
2024-02-01 13:50:43 +0300

    import testimonial page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
+import Testimonial from '../components/Testimonial/Testimonial'
 
 function HomePage() {
   return (
     <div>
       <Hero />
+      <Testimonial />
     </div>
   )
 }
