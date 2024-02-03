149d74fe76cb52d26de1d568ce14ef46a4be771f
Yahya <yahyahussein670@gmail.com>
2024-02-03 15:11:45 +0300

    update brand logos

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import sponsor1 from '../../assets/images/Testimonial/sponsor1.png'
+import sponsor8 from '../../assets/images/Testimonial/sponsor8.png'
 import sponsor2 from '../../assets/images/Testimonial/sponsor2.png'
-import sponsor3 from '../../assets/images/Testimonial/sponsor3.png'
-import sponsor4 from '../../assets/images/Testimonial/sponsor4.png'
+import sponsor7 from '../../assets/images/Testimonial/sponsor7.png'
+import sponsor9 from '../../assets/images/Testimonial/sponsor9.png'
 import './testimonial.css'
 
 function Testimonial() {
   return (
     <div id='test' className='flex items-center justify-between md:mt-6 space-x-8 overflow-x-auto md:overflow-x-hidden '>
-        <img src={sponsor1} alt="" loading='lazy' />
+        <img src={sponsor8} alt="" loading='lazy' />
         <img src={sponsor2} alt="" loading='lazy' />
-        <img src={sponsor3} alt="" loading='lazy' />
-        <img src={sponsor4} alt="" loading='lazy'/>
+        <img src={sponsor7} alt="" loading='lazy' />
+        <img src={sponsor9} alt="" loading='lazy'/>
     </div>
     
   )
